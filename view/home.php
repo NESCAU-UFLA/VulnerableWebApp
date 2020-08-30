bd66b4a500307deb8c638783bec86c6012797d34
Wixmix <vitor.borges@computacao.ufla.br>
2020-08-30 14:39:53 -0300

    Adicionando a child page

--- view/home.php
+++ view/home.php
@@ -1,8 +1,8 @@
 <?php
     include_once("../model/Usuario.php");
     session_start();
+    $_SESSION['isChild'] = true;
     if (isset($_SESSION['usuario'])) {
-        $user = unserialize($_SESSION['usuario']);
 ?>
         <html>
             <head>
@@ -10,34 +10,11 @@
                 <title>Home</title>
             </head>
             <body>
-                <div style="display: flex;">
-                    <div class="column" style="width: 200px; height: 300px; background-color: white;">
-                        a
-                    </div>
-                    <div class="column" style="width: 300px; height: 300px; background-color: white;">
-                        a
-                    </div>
-                    <div class="column" style="width: 200px; height: 300px; background-color: white;">
-                        <div class="user-img" style="margin-top: 10px;">
-<?php                       echo '<img src="../uploads/'.$user->getFoto().'" />'; ?>
-                        </div>
-                        <h3>
-<?php                       echo "Bem-vindo, ".$user->getNome()."!";?>
-                        </h3><br/>
-                        <div>
-                            <a href="editar-dados.php">
-                                <button>Ver perfil</button>
-                            </a><br/><br/>
-                            <form method="POST" action="../controller/usuario.php">
-                                <input type="hidden" name="Usuario" value="Logout" />
-                                <button type="submit">Desconectar</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+                <?php include("main-home.php"); ?>
             </body>
         </html>
 <?php
+    unset($_SESSION['isChild']);
     } else
         header("Location: index.php");
 ?>
--- view/main-home.php
+++ view/main-home.php
@@ -1,39 +1,30 @@
 <?php
-    session_start();
     $user = unserialize($_SESSION['usuario']);
+    if (!isset($_SESSION['isChild']))
+        header("Location: index.php");
 ?>
-<html>
-    <head>
-        <script type="text/javascript" src="../modules/jquery-3.5.1.min.js"></script>
-        <script type="text/javascript" src="../modules/functions.js"></script>
-        <link href="../modules/style.css" rel="stylesheet" type="text/css" />
-        <title>Home</title>
-    </head>
-    <body>
-        <div style="display: flex;">
-            <div class="column" style="width: 200px; height: 300px; background-color: white;">
-                a
-            </div>
-            <div class="column" style="width: 300px; height: 300px; background-color: white;">
-                a
-            </div>
-            <div class="column" style="width: 200px; height: 300px; background-color: white;">
-                <div class="user-img" style="margin-top: 10px;">
-        <?php       echo '<img src="../uploads/'.$user->getFoto().'" />'; ?>
-                </div>
-                <h3>
-        <?php       echo "Bem-vindo, ".$user->getNome()."!";?>
-                </h3><br/>
-                <div>
-                    <a href="editar-dados.php">
-                        <button>Ver perfil</button>
-                    </a><br/><br/>
-                    <form method="POST" action="../controller/usuario.php">
-                        <input type="hidden" name="Usuario" value="Logout" />
-                        <button type="submit">Desconectar</button>
-                    </form>
-                </div>
-            </div>
+<div style="display: flex;">
+    <div class="column" style="width: 200px; height: 300px; background-color: white;">
+        a
+    </div>
+    <div class="column" style="width: 300px; height: 300px; background-color: white;">
+        a
+    </div>
+    <div class="column" style="width: 200px; height: 300px; background-color: white;">
+        <div class="user-img" style="margin-top: 10px;">
+<?php       echo '<img src="../uploads/'.$user->getFoto().'" />'; ?>
         </div>
-    </body>
-</html>
+        <h3>
+<?php       echo "Bem-vindo, ".$user->getNome()."!";?>
+        </h3><br/>
+        <div>
+            <a href="editar-dados.php">
+                <button>Ver perfil</button>
+            </a><br/><br/>
+            <form method="POST" action="../controller/usuario.php">
+                <input type="hidden" name="Usuario" value="Logout" />
+                <button type="submit">Desconectar</button>
+            </form>
+        </div>
+    </div>
+</div>

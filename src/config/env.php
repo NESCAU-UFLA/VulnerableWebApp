12712354a078ccf9367b3a97fdfc1b515226cab2
Wixmix <vitor.borges@computacao.ufla.br>
2021-03-09 09:45:09 -0300

    Subindo novos arquivos de configuracao.

---
+++ src/config/env.php
@@ -0,0 +1,10 @@
+<?php
+// Configuração do banco
+$_ENV['DB_HOST'] = '';
+$_ENV['DB_USER'] = '';
+$_ENV['DB_PASS'] = '';
+$_ENV['DB_NAME'] = '';
+
+// Configuração de diretório
+$_ENV['USER_IMG_PATH'] = '';
+?>
